284cac126d4b20bb89fee4797f1cbb6555df9887
Pedro Luis <pedrolssilva@hotmail.com>
2023-08-22 19:06:39 -0300

    created alura-tracker project and implemented some components

---
+++ src/components/Formulario.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+        />
+      </div>
+      <div class="column">
+        <TemporizadorTimer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TemporizadorTimer from './TemporizadorTimer.vue'
+
+export default defineComponent({
+  name: 'FormularioTask',
+  components: {
+    TemporizadorTimer,
+  },
+})
+</script>
---
+++ src/components/TemporizadorTimer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <CronometroCounter :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CronometroCounter from './CronometroCounter.vue'
+
+export default defineComponent({
+  name: 'TemporizadorCounter',
+  components: {
+    CronometroCounter,
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+      cronometroRodando: false,
+    }
+  },
+  methods: {
+    iniciar() {
+      this.cronometroRodando = true
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
+      console.log('iniciando')
+    },
+    finalizar() {
+      this.cronometroRodando = false
+      clearInterval(this.cronometro)
+    },
+  },
+})
+</script>
