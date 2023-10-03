dfddee4e134eafb0d3c0b87dcc1f5d184e2f2ea7
Pedro L. (@pedrolssilva) <pedrolssilva@hotmail.com>
2023-10-03 19:28:14 -0300

    using vue-router dependency and setting router file

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -3,7 +3,25 @@
     <h1>
       <img src="../assets//logo.png" alt="" />
     </h1>
-    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    <div class="has-text-centered">
+      <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -35,18 +53,40 @@ export default defineComponent({
 </script>
 
 <style scoped>
+h1 {
+  text-align: center;
+}
+strong {
+  color: #f95738;
+}
 header {
-  padding: 1rem;
+  padding: 2rem;
   background: #0d3b66;
   width: 100%;
   height: 100vh;
-  text-align: center;
 }
 
 @media only screen and (max-width: 768px) {
   header {
-    padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel {
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+}
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #faf0ca;
+}
+.link.router-link-active {
+  color: #faf0ca;
 }
 </style>
