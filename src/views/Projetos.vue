ebe63dc05466006a4e58033c396876c5c62431d1
Pedro L. (@pedrolssilva) <pedrolssilva@hotmail.com>
2023-10-05 21:13:48 -0300

    Extracted new and edit project from Project view

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,31 +1,31 @@
 <template>
   <section class="projetos">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label"> Nome do projeto </label>
-        <input
-          type="text"
-          class="input"
-          v-model="nomeDoProjeto"
-          id="nomeDoProjeto"
-        />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
     <table class="table is-fullwidth">
       <thead>
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" v-bind:key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projeto/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -38,21 +38,9 @@ import { useStore } from '@/store'
 
 export default defineComponent({
   name: 'Projetos',
-  data() {
-    return {
-      nomeDoProjeto: '',
-    }
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-      this.nomeDoProjeto = ''
-    },
-  },
   setup() {
     const store = useStore()
     return {
-      store,
       projetos: computed(() => store.state.projetos),
     }
   },
